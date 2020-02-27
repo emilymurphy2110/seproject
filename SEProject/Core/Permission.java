5df1ad87f0fa9e23d16c4ecebed71a4ed13b01fa
HeatherAB <heatherboyle12@yahoo.com>
2020-02-27 18:22:57 +0000

    Added ClassSkills class/ClassSkillsMatch enum
    Edited Permissions and Roles

--- SEProject/Core/Permission.java
+++ SEProject/Core/Permission.java
@@ -3,19 +3,21 @@ package Core;
 public enum Permission {
 	
 	  ADD_ENTRY("Add entry"),
-	    UPDATE_ENTRY("Update entry"),
-	    REMOVE_ENTRY("Remove entry"),
-	    ACCEPT_REQUEST("..."),
-	    VIEW_REQUEST("...");
-
-	    private String title;
-
-	    Permission(String title) {
-	        this.title = title;
-	    }
-
-	    public String getTitle() {
-	        return title;
-	    }
-
+	  UPDATE_ENTRY("Update entry"),
+	  REMOVE_ENTRY("Remove entry"),
+	  CREATE_REQUEST("Create request"),
+	  VIEW_REQUEST("View request"),
+	  ACCEPT_REQUEST("Accept request"),
+	  DENY_REQUEST("Deny request");
+	  
+	
+	private String title;
+	
+	Permission(String title) {
+		this.title = title;
+		}
+	
+	public String getTitle() {
+		return title;
+		}
 }
